2c4785c51412529db016a8181bf812db51934f2c
basicpalkovic <palkovic44@gmail.com>
2023-01-24 20:47:25 +0100

    css files changes

--- profile.php
+++ profile.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/constants.css">
+    <link rel="stylesheet" href="css/profile.css">
+    <title>Cobra Weather</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&700display=swap" rel="stylesheet">
+</head>
+
 <?php require 'constants/header.php'; ?>
 <div class="show-profile-info">
     <div class="profile-img-holder">
--- search.php
+++ search.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/constants.css">
+    <link rel="stylesheet" href="css/search.css">
+    <title>Cobra Weather</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&700display=swap" rel="stylesheet">
+</head>
 <?php
 // Include Request and Response classes
 $api_key = '317875d9da97449bd31293b89a4d994e';
@@ -39,6 +54,8 @@ var_dump($response_data_api_ll);*/
 //print_r($user_data);
 
 // Traverse array and display user data
+
+
 require 'constants/header.php';
 ?>
 <div class="content-my">
